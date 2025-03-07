3f09d9106dba89e44fa2281b8d5bf28576d1d295
Levin Mathew Abraham <levinabraham2819@gmail.com>
2025-03-07 19:50:09 +0530

    Revert "Update config.js"
    This reverts commit 9988d6af453e0458738c6c2446bf0b1bc559217a.

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 const PORT = process.env.PORT || 5000;
 const CLIENT_URL = process.env.NODE_ENV === 'production'
   ? 'https://mental-health-app-flax-five.vercel.app/'  // Replace with your actual production client URL
-  : 'http://localhost:5000';
+  : 'http://localhost:3000';
 
 module.exports = { PORT, CLIENT_URL };
