01fc000951a0a594b72b7f7750e2a648ba7a808d
Levin Mathew Abraham <levinabraham2819@gmail.com>
2025-03-08 10:50:26 +0530

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 const PORT = process.env.PORT || 5000;
 const CLIENT_URL = process.env.NODE_ENV === 'production'
-  ? 'https://mental-health-app-flax-five.vercel.app/'  // Replace with your actual production client URL
+  ? 'https://mental-health-app-frontend.onrender.com//'  // Replace with your actual production client URL
   : 'http://localhost:3000';
 
 module.exports = { PORT, CLIENT_URL };
